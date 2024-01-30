e8e6560ebfb6df1cf1013c2d6a40589b88d406d1
Vishal Maurya <vishalmaurya12nov@gmil.com>
2024-01-30 13:12:58 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,103 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const multer = require("multer");
+// const path = require("path");
+// const axios = require("axios");
+
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// app.use(bodyParser.json());
+// app.use(cors());
+// // mount public folder
+// // app.use("/", express.static("public"));
+
+
+// app.use("/static", express.static(path.join(__dirname, "public")));
+
+// // multer
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// app.get("/api/sendMessage", (req, res) => {
+//   res.send(`<h1>APi is working </h1>`);
+// });
+// app.post("/api/sendMessage", upload.single("image"), (req, res) => {
+//   const { phone, selectedOption, message, caption } = req.body;
+
+//   const liveUrl = req.file
+//     ? `https://whatsapp-backend-blue.vercel.app/${req.file.filename}`
+//     : null;
+
+//   console.log("Received message data:", {
+//     phone,
+//     selectedOption,
+//     message,
+//     caption,
+//   });
+
+//   // let data = null;
+
+//   // if (selectedOption === "text") {
+//   //   data = {
+//   //     messaging_product: "whatsapp_business",
+//   //     to: phone,
+//   //     type: "text",
+//   //     text: {
+//   //       body: message,
+//   //     },
+//   //   };
+//   // } else if (selectedOption === "image" || selectedOption === "video" || selectedOption === "document") {
+//   //   data = {
+//   //     messaging_product: "whatsapp_business",
+//   //     to: phone,
+//   //     type: selectedOption,
+//   //     [selectedOption]: {
+//   //       link: liveUrl,
+//   //       caption: caption,
+//   //     },
+//   //   };
+//   // }
+
+//   // const url = "https://graph.facebook.com/v18.0/239423185911953/messages";
+//   // const accessToken =
+//   //   "EAAQ4gWEoyicBO3pgpJZAGaLrHoAAmjyNEiP4bZAcNb7afXlRoBR7oMEg2bvFTUbIM8dwoe2TQkZBuFU3k6rtJvdpTyCLRq5zlCgzZApaKq2YnTwPbZAveD9OmTXQDgUHBtVqk9d6yz6ALLH4t32FjWnZCb4fiYjoNEnZBFW6uvNzZAqYoZAzDWQ5vftR2erhWSoktbugz0xxO5luGfKPmDKclfcBuEs9fl8W14w0ZD";
+
+//   // const headers = {
+//   //   Authorization: `Bearer ${accessToken}`,
+//   //   "Content-Type": "application/json",
+//   // };
+
+//   // axios
+//   //   .post(url, data, { headers })
+//   //   .then((response) => {
+//   //     console.log("Response:", response.data);
+//   //   })
+//   //   .catch((error) => {
+//   //     console.error(
+//   //       "Error:",
+//   //       error.response ? error.response.data : error.message
+//   //     );
+//   //   });
+
+//   res.json({ success: true, message: liveUrl });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -10,17 +110,15 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
-// mount public folder
-// app.use("/", express.static("public"));
 
-
+// Serve static files from the "public" folder
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 // multer
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/");
+    cb(null, path.join(__dirname, "public"));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
@@ -30,13 +128,14 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.get("/api/sendMessage", (req, res) => {
-  res.send(`<h1>APi is working </h1>`);
+  res.send(`<h1>API is working</h1>`);
 });
+
 app.post("/api/sendMessage", upload.single("image"), (req, res) => {
   const { phone, selectedOption, message, caption } = req.body;
 
   const liveUrl = req.file
-    ? `https://whatsapp-backend-blue.vercel.app/${req.file.filename}`
+    ? `https://whatsapp-backend-blue.vercel.app/static/${req.file.filename}`
     : null;
 
   console.log("Received message data:", {
@@ -46,49 +145,6 @@ app.post("/api/sendMessage", upload.single("image"), (req, res) => {
     caption,
   });
 
-  // let data = null;
-
-  // if (selectedOption === "text") {
-  //   data = {
-  //     messaging_product: "whatsapp_business",
-  //     to: phone,
-  //     type: "text",
-  //     text: {
-  //       body: message,
-  //     },
-  //   };
-  // } else if (selectedOption === "image" || selectedOption === "video" || selectedOption === "document") {
-  //   data = {
-  //     messaging_product: "whatsapp_business",
-  //     to: phone,
-  //     type: selectedOption,
-  //     [selectedOption]: {
-  //       link: liveUrl,
-  //       caption: caption,
-  //     },
-  //   };
-  // }
-
-  // const url = "https://graph.facebook.com/v18.0/239423185911953/messages";
-  // const accessToken =
-  //   "EAAQ4gWEoyicBO3pgpJZAGaLrHoAAmjyNEiP4bZAcNb7afXlRoBR7oMEg2bvFTUbIM8dwoe2TQkZBuFU3k6rtJvdpTyCLRq5zlCgzZApaKq2YnTwPbZAveD9OmTXQDgUHBtVqk9d6yz6ALLH4t32FjWnZCb4fiYjoNEnZBFW6uvNzZAqYoZAzDWQ5vftR2erhWSoktbugz0xxO5luGfKPmDKclfcBuEs9fl8W14w0ZD";
-
-  // const headers = {
-  //   Authorization: `Bearer ${accessToken}`,
-  //   "Content-Type": "application/json",
-  // };
-
-  // axios
-  //   .post(url, data, { headers })
-  //   .then((response) => {
-  //     console.log("Response:", response.data);
-  //   })
-  //   .catch((error) => {
-  //     console.error(
-  //       "Error:",
-  //       error.response ? error.response.data : error.message
-  //     );
-  //   });
 
   res.json({ success: true, message: liveUrl });
 });
@@ -96,3 +152,7 @@ app.post("/api/sendMessage", upload.single("image"), (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+
